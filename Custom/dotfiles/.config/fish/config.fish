201a0bb91efa6a9eed2f866f754dc8655cd95322
Eric Sandu <eric_andrei.sandu@stud.acs.upb.ro>
2026-07-10 00:33:26 +0300

    feat(shell): add bat and fd aliases for fish

--- Custom/dotfiles/.config/fish/config.fish
+++ Custom/dotfiles/.config/fish/config.fish
@@ -1,4 +1,39 @@
 # All User Configs goes here
 
-# Added by Antigravity CLI installer
-set -gx PATH "/home/phanter/.local/bin" $PATH
+# Environment Variables
+set -gx CFLAGS "-g -O2 -march=native -pipe"
+set -gx CXXFLAGS "-g -O2 -march=native -pipe"
+set -gx EDITOR nvim
+set -gx CUPS_SERVER "192.168.1.32:631"
+set -gx GAMEID 0
+set -gx PROTONPATH "$HOME/.steam/steam/compatibilitytools.d/GE-Proton10-28/"
+set -gx WINEPREFIX "$HOME/.wine"
+
+# Path additions
+fish_add_path ~/wine-ge/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/.local/share/bin
+
+set -gx ZEPHYR_SDK_INSTALL_DIR "/opt/zephyr-sdk-1.0.1"
+set -gx ZEPHYR_TOOLCHAIN_VARIANT "zephyr"
+
+# Aliases
+# (Note: standard list directory and navigate aliases are already in hyde.fish)
+alias pc='yay -Sc'
+alias po='yay -Qtdq | yay -Rns -'
+alias ssh='kitten ssh'
+alias nf='fastfetch'
+alias neofetch='fastfetch'
+alias vi='nvim'
+alias vim='nvim'
+alias cat='bat'
+alias find='fd'
+
+# Shell Enhancements
+# Enable Fish Vi Mode (equivalent to zsh-vi-mode plugin)
+fish_vi_key_bindings
+
+# Startup command integrations
+if type -q thefuck
+    thefuck --alias | source
+end
